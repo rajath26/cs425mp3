972d50c5089e50255e200e57527d4c97b668b7ac
rajath26 <rajath26@gmail.com>
2013-11-10 01:21:36 -0600

    changed compile script

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-gcc host.c `pkg-config --cflags --libs glib-2.0` -lpthread
 
-gcc KVclient.c `pkg-config --cflags --libs glib-2.0` -lpthread
+gcc ./src/host.c -o ./src/host `pkg-config --cflags --libs glib-2.0` -lpthread 
+
+gcc ./src/KVclient.c -o ./src/KVclient `pkg-config --cflags --libs glib-2.0` -lpthread
 
 
