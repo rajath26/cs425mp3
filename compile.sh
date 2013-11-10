e6f8d05bd46bd79b85208379236b8a5b431a4a48
rajath26 <rajath26@gmail.com>
2013-11-10 01:38:13 -0600

    some changes by grk

--- compile.sh
+++ compile.sh
@@ -3,6 +3,16 @@
 
 gcc ./src/host.c -o ./src/host `pkg-config --cflags --libs glib-2.0` -lpthread 
 
+if [ $? -eq 0 ]
+then 
+    echo -e "\nServer compiled\n"
+fi
+
 gcc ./src/KVclient.c -o ./src/KVclient `pkg-config --cflags --libs glib-2.0` -lpthread
+if [ $? -eq 0 ]
+then
+    echo -e "\nClient compiled\n"
+fi
+
 
 
